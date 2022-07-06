632967f558e105a4a7df81ba8fbfeb187e0457c5
Levi <levik885@gmail.com>
2022-07-06 10:48:45 -0400

    git commit

---
+++ script.js
@@ -0,0 +1,58 @@
+function contactB() {
+  var element = document.querySelector('.aboutB');
+    element.style.backgroundColor = "#333";
+
+    var element = document.querySelector('.projectB');
+    element.style.backgroundColor = "#333";
+
+    var element = document.querySelector('.homeB');
+    element.style.backgroundColor = "#333";
+
+    var element = document.querySelector('.contactB');
+    element.style.backgroundColor = "blue";
+}
+
+function aboutB() {
+    var element = document.querySelector('.aboutB');
+    element.style.backgroundColor = "blue";
+
+    var element = document.querySelector('.projectB');
+    element.style.backgroundColor = "#333";
+
+    var element = document.querySelector('.homeB');
+    element.style.backgroundColor = "#333";
+
+    var element = document.querySelector('.contactB');
+    element.style.backgroundColor = "#333";
+}
+
+function projectB() {
+    var element = document.querySelector('.aboutB');
+    element.style.backgroundColor = "#333";
+
+    var element = document.querySelector('.projectB');
+    element.style.backgroundColor = "blue";
+
+    var element = document.querySelector('.homeB');
+    element.style.backgroundColor = "#333";
+
+    var element = document.querySelector('.contactB');
+    element.style.backgroundColor = "#333";
+
+}
+
+function homeB() {
+    var element = document.querySelector('.aboutB');
+    element.style.backgroundColor = "#333";
+
+    var element = document.querySelector('.projectB');
+    element.style.backgroundColor = "#333";
+
+    var element = document.querySelector('.homeB');
+    element.style.backgroundColor = "blue";
+
+    var element = document.querySelector('.contactB');
+    element.style.backgroundColor = "#333";
+
+}
+
